c8919162670904c88a142006a7712f942f0065b5
Mikhail Antonov <mikhailantonov@Mikhails-MacBook-Air-2.local>
2024-03-26 22:50:09 +0400

    updated contract address

--- crypto_vault/config.py
+++ crypto_vault/config.py
@@ -3,7 +3,7 @@ from eth_typing import Address
 
 
 class Settings(BaseModel):
-    CONTRACT_ADDRESS: Address = "0x1404951233eCE16010D728F67C8F9b7D8B40fB0e"
+    CONTRACT_ADDRESS: Address = "0x276c89d95300b11b8ACEAE24a2dbc4bc3BaB69F5"
     ABI_FILENAME: str = "abi.json"
 
 
